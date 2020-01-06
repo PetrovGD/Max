19c82b88578e87c0935ff1893e70b2a15981d69e
PetrovGD <59577696+PetrovGD@users.noreply.github.com>
2020-01-06 20:46:41 +0200

    Отворения код на приложението

---
+++ max.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+    char cln;
+    int a,b,c;
+    while(1){
+    cout << "a=" << endl;
+    cin >> a;
+    cout << "b="<< endl;
+    cin >> b;
+    cout << "c="<< endl;
+    cin >> c;
+    if(a>b){
+        if(a>c){
+            cout << "The max is " << a;
+        }else{
+            cout << "The max is " << c;
+        }
+    }
+    else{
+        if(b>c)
+            cout <<  "The max is " << b;
+        else
+            cout << "The max is " << c;
+    }
+    cln=getch();
+        if(cln=='c'||cln=='C'){
+            system("cls");
+        }
+    }
+}
+
+//Copyright Georgi Petrov
